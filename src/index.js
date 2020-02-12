c780b8eef1261d43049d76aa0ac1bf255c04d9c6
alexalrobinson <alexa.l.robinson@gmail.com>
2020-02-11 20:34:06 -0500

    rough draft

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import('bootstrap');
+import './styles/main.scss';
+import logo from './images/lock_starlogo.jpg';
+import van from './images/van.jpg';
+import carousel1 from './images/carousel1.jpg';
+import carousel2 from './images/carousel2.jpg';
+import carousel3 from './images/carousel3.jpg';
+import carousel4 from './images/carousel4.jpg';
+
+document.getElementById("logo").src =logo;
+document.getElementById("carousel1").src = carousel1;
+document.getElementById("carousel2").src = carousel2;
+document.getElementById("carousel3").src = carousel3;
+document.getElementById("carousel4").src = carousel4;
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const htmlPlugin = require("html-webpack-plugin");
+const miniCSSPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|ico)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                        loader: 'style-loader', // inject CSS to page
+                    }, {
+                        loader: 'css-loader', // translates CSS into CommonJS modules
+                    }, {
+                        loader: 'postcss-loader', // Run post css actions
+                        options: {
+                            plugins: function () { // post css plugins, can be exported to postcss.config.js
+                                return [
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }, {
+                        loader: 'sass-loader' // compiles Sass to CSS
+                }]
+            }
+        ],
+    },
+    plugins: [
+        new htmlPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new miniCSSPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
