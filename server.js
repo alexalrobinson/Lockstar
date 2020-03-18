6bfde810b3819e50d37c0e7a4a582c7a6c0a8e23
alexalrobinson <alexa.l.robinson@gmail.com>
2020-03-18 17:24:33 -0400

    changes to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 
 //const request = require('request');
 
@@ -35,6 +36,8 @@ const addPost = (request, response) => {
 }
 
 // the __dirname is the current directory from where the script is running
+//app.engine('handlebars', exphbs());
+//app.set('view engine', 'handlebars');
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
@@ -52,6 +55,52 @@ app
   .get(getPosts)
   // POST endpoint
   .post(addPost)
+
+/*app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM posts');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})*/
+
+app.post('/submit',function(req,res){
+  // g-recaptcha-response is the key that browser will generate upon form submit.
+  // if its blank or null means user has not selected the captcha, so return the error.
+  res.setHeader("Content-Security-Policy", "default-src 'self' www.google.com www.gstatic.com 'unsafe-inline' 'unsafe-eval' data:;");
+  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+    return res.json({"responseCode" : 1,"responseDesc" : "Please select captcha"});
+  }
+  // Put your secret key here.
+  var secretKey = process.env.SECRET_KEY;
+  // req.connection.remoteAddress will provide IP address of connected user.
+  var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret="+secretKey+"&response="+req.body['g-recaptcha-response']+"&remoteip="+req.connection.remoteAddress;
+  // Hitting GET request to the URL, Google will respond with success or error scenario.
+  request(verificationUrl,function(error,response,body) {
+    body = JSON.parse(body);
+    // Success will be true or false depending upon captcha validation.
+    if(body.success !== undefined && !body.success) {
+      return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
+    }
+    res.json({"responseCode" : 0,"responseDesc" : "Sucess"});
+  });
+});
+/*
+app.post("/api/comments", function (request, response) {
+  var userName = request.body.name;
+  var doc = { "name" : userName };
+  if(!mydb) {
+    console.log("No database.");
+    response.send(doc);
+    return;
+  }
+  newComment.cloudant(doc, response);
+});*/
 
 // This will handle 404 requests.
 app.use("*",function(req,res) {
