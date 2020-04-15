a1ff816b7b277b6c9cf7cd78097bdb08de4a149a
alexalrobinson <alexa.l.robinson@gmail.com>
2020-04-15 14:52:25 -0400

    mess with recaptcha

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const recaptcha = require('express-recaptcha');
 
 
 const port = process.env.PORT || 8080;
