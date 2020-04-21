e6115cd90bb183b49129ef2bb7a7a289aa74611b
alexalrobinson <alexa.l.robinson@gmail.com>
2020-04-21 01:11:57 -0400

    aesthetic changes

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import carousel1 from './images/carousel1.jpg';
 import carousel2 from './images/carousel2.jpg';
 import carousel3 from './images/carousel3.jpg';
 import carousel4 from './images/carousel4.jpg';
+import aloa from './images/aloa.jpeg';
 
-document.getElementById("logo").src =logo;
+document.getElementById("logo").src = logo;
+document.getElementById("van").src = van;
 document.getElementById("carousel1").src = carousel1;
 document.getElementById("carousel2").src = carousel2;
 document.getElementById("carousel3").src = carousel3;
 document.getElementById("carousel4").src = carousel4;
+document.getElemenyById("aloa").src = aloa;
 
 let posts;
 let postsToShow=5;
